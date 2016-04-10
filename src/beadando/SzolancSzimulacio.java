54af69b084943a5a0ee698c61a4614548d35c972
Vukasin <vukasindfelbab@gmail.com>
2016-04-10 20:55:48 +0200

    bugfix

--- src/beadando/SzolancSzimulacio.java
+++ src/beadando/SzolancSzimulacio.java
@@ -5,10 +5,80 @@
  */
 package beadando;
 
+import java.io.IOException;
+
 /**
  *
  * @author fevtabb
  */
 public class SzolancSzimulacio {
-    
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ServerThread st = new ServerThread();
+        st.start();
+
+        Thread.sleep(100);
+
+        AI ai1 = new AI("Jatekos1", "szokincs1.txt");
+        ai1.startAI();
+
+        Thread.sleep(200);
+
+        AI ai2 = new AI("Jatekos2", "szokincs1.txt");
+        ai2.startAI();
+
+        Thread.sleep(200);
+
+        AI ai3 = new AI("Jatekos3", "szokincs1.txt");
+        ai3.startAI();
+
+        Thread.sleep(200);
+
+        AI ai4 = new AI("Jatekos4", "szokincs2.txt");
+        ai4.startAI();
+
+    }
+
+    private static class ServerThread extends Thread {
+
+        GameServer gs = null;
+
+        ServerThread () {
+            try {
+                gs = new GameServer();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run(){
+            try {
+                gs.startServer();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class AIThread extends Thread {
+        AI ai = null;
+
+        AIThread(String name, String words){
+            ai = new AI(name, words);
+        }
+
+        @Override
+        public void run(){
+            try {
+                ai.startAI();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
